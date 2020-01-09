ec7944ba92449e0065eb430693765a3edfe0e4a1
Gorm Lai <gorm@kotoristudios.com>
2020-01-09 21:30:52 +0000

    updated

---
+++ shaders/glsl/triangle.vert
@@ -0,0 +1,17 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(binding = 0) uniform UniformBufferObject {
+    mat4 _model;
+    mat4 _view;
+    mat4 _proj;
+} ubo;
+
+layout(location = 0) in vec4 inPosition;
+layout(location = 1) in vec4 inColor;
+layout(location = 0) out vec4 fragColor;
+
+void main() {  
+  gl_Position = ubo._proj * ubo._view * ubo._model * inPosition;
+  fragColor = inColor;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "singlefile/CircularArray.h"
+#include "singlefile/SDL_FileStream.h"
 #include "vulkan-setup/VulkanSetup.h"
 
 namespace
@@ -32,9 +33,37 @@ namespace
         }
         
         return window;
-        
     }
+
+    std::vector<char> readFile(const std::string file)
+    {
+        SDL_FileStream stream(file, (unsigned int)SDL_FileStream::OpenFlags::READ_ONLY | (unsigned int)SDL_FileStream::OpenFlags::BINARY);
+
+        std::vector<char> data;
+        stream.read(data);
+        return data;
+    }
+
+    bool readShaders(std::vector<Vulkan::Shader>& shaders)
+    {
+        SDL_LogInfo(0, "Loading Shaders!\n");
+        unsigned int numShaders = (unsigned int)shaders.size();
+        for (unsigned int i = 0; i < numShaders; i++)
+        {
+            const std::string& shaderName = shaders[i]._filename;
+            SDL_LogInfo(0, "\tLoading Shader: %s!\n", shaderName.c_str());
+
+            std::vector<char> byteCode = readFile(shaderName);
+            if (!byteCode.empty())
+                shaders[i]._byteCode = byteCode;
+        }
+        return true;
+    }
+
+
 }
+
+
 
 
 int main(int argc, char *argv[])
@@ -43,9 +72,67 @@ int main(int argc, char *argv[])
 
     SDL_Window * window = initSDL(appName);
 
-    Vulkan::AppDescriptor appDescriptor;
-    appDescriptor._appName = appName;
-    appDescriptor._requiredVulkanVersion = VK_API_VERSION_1_1;
+    Vulkan::AppDescriptor appDesc;
+    appDesc._appName = appName;
+    appDesc._requiredVulkanVersion = VK_API_VERSION_1_1;
+    appDesc._window = window;
+
+    std::vector<Vulkan::Shader> shaders =
+    {
+        {"triangle_vert.spv", Vulkan::ShaderType::Vertex},
+        {"triangle_frag.spv", Vulkan::ShaderType::Fragment},
+    };
+    readShaders(shaders);
+    appDesc._shaders = shaders;
+
+
+    glm::vec3 camPos{ 0,0,8 };
+    glm::vec3 camDir{ 0,0,-1 };
+    glm::vec3 camUp{ 0,1,0 };
+    appDesc._cameraUpdateFunction = [&camPos, &camDir, &camUp](glm::vec3& pos, glm::vec3& lookat, glm::vec3& up)
+    {
+        pos = camPos;
+        lookat = camPos + camDir;
+        up = camUp;
+    };
+
+
+    //    Mesh mesh = MeshFactory::createPlane(1.0f, 1.0f);
+
+    appDesc._backgroundClearColor = []() { return glm::vec4{ 0.2f, 0.2f, 0.2f, 1.0 }; };
+
+    glm::quat totalRot;
+    appDesc._updateModelMatrix = [&totalRot](const void* userData, float timePassed, float deltaTime)
+    {
+        glm::mat4 rVal;
+        if (userData != nullptr)
+        {
+            const Mesh* mesh = static_cast<const Mesh*>(userData);
+            rVal = mesh->getWorldMatrix();
+        }
+
+        return rVal;
+    };
+
+    appDesc._getBindingDescription = []() { return MeshVertex::getBindingDescription(); };
+    appDesc._getAttributes = []() { return MeshVertex::getAttributes(); };
+
+    appDesc._updateFunction = [&totalRot](float timePassed, float deltaTime)
+    {
+        glm::quat xRot = glm::rotate(timePassed * 0.8f * glm::half_pi<float>(), glm::vec3(1.0f, 0.0f, 0.0f));
+        glm::quat yRot = glm::rotate(timePassed * 0.6f * glm::half_pi<float>(), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::quat zRot = glm::rotate(timePassed * glm::half_pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f));
+        //        totalRot = xRot * yRot * zRot;
+        return true;
+    };
+
+
+    Vulkan::VulkanContext context;
+    if(!Vulkan::handleVulkanSetup(appDesc, context))
+    {
+        SDL_LogError(0, "Failed to handle vulkan setup\n");
+        return 1;
+    }
 
     // creating a renderer
     SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
